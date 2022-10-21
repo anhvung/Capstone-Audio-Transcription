776cb8344ab08fd22a3672da08494269a209a79b
anvdn <antonin.vdn@gmail.com>
2022-10-21 21:43:43 +0000

    noise for range 1-6%

--- wav2vec/utils.py
+++ wav2vec/utils.py
@@ -1,6 +1,8 @@
+import colorednoise as cn
 from datasets import load_dataset, load_from_disk
 from jiwer import wer
 import librosa
+import numpy as np
 import os
 import tarfile
 import torch
@@ -81,10 +83,73 @@ def down_sample(s, sample_rate=16000, output_sr=8000):
     # output should be at 16kHz sample rate
     return noisy_s
 
+
+def add_noise(s, sample_rate=16000, noise_percentage_factor = .01, noise_type='white'):
+    # s: audio input (mono)
+    # sample rate: sample rate of s
+    # noise_percentage_factor, percentage scale of added noise added
+    # type: white, pink, brown
+
+    if noise_type == 'white':
+        beta = 0
+
+    elif noise_type == 'pink':
+        beta = 1
+
+    elif noise_type == 'brown':
+        beta = 2
+
+    noise = cn.powerlaw_psd_gaussian(beta, s.size)
+    noisy_s = s + noise * noise_percentage_factor
+
+    # output should be at 16kHz sample rate
+    if sample_rate != 16000:
+        noisy_s = librosa.resample(noisy_s, orig_sr = sample_rate, target_sr=16000)
+
+    return noisy_s
+
+
+def add_signals(s, back_s, sample_rate=16000, back_sample_rate=16000, noise_db=-12):
+    # s: audio input (mono)
+    # back_s: brckgrnd audio
+    # sample rate: sample rate of s
+    # noise_db: lower the backgrnd signal by noise_db db
+
+
+    # make sure both signals have same 16kHz sample rate
+    if sample_rate != 16000:
+        s = librosa.resample(s, orig_sr=sample_rate, target_sr=16000)
+
+    if back_sample_rate != 16000:
+        back_s = librosa.resample(back_s, orig_sr=back_sample_rate, target_sr=16000)
+
+    if s.size > back_s.size:
+        back_s = librosa.util.pad_center(back_s, size=s.size)
+
+    elif s.size < back_s.size:
+        s = librosa.util.pad_center(s, size=back_s.size)
+
+    # lower background signal by noise_db
+    noise_amp = librosa.db_to_amplitude(noise_db)
+    lower_back_s = back_s - noise_amp
+
+    # add background noise to sound clip
+    noisy_s = s + back_s
+
+    # output should be at 16kHz sample rate
+    return noisy_s
+
 def map_to_downsampled(batch, input_sr=16000, output_sr=8000):
     """
     map to downsampled audio array
     """
     batch['audio']['array'] = down_sample(batch['audio']['array'], input_sr, output_sr)
     batch['audio']['sampling_rate'] = output_sr
+    return batch
+
+def map_to_noisy(batch, sample_rate=16000, noise_percentage_factor = .01, noise_type='white'):
+    """
+    map to downsampled audio array
+    """
+    batch['audio']['array'] = add_noise(batch['audio']['array'], sample_rate=sample_rate, noise_percentage_factor = noise_percentage_factor, noise_type=noise_type)
     return batch
